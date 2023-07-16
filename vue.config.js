a6114630e14e8c0168d5d8c9256d0ce08451f8ab
yujeong-world <124220083+yujeong-world@users.noreply.github.com>
2023-07-16 19:36:34 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: '/barkey-project-frontend/'
+  publicPath: '/barkey-project-frontend/',
+  devServer: {
+    historyApiFallback: true, // 서브 디렉토리 라우팅을 위한 설정
+  },
 })
